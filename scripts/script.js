7bc507bc8ad55377862a9d03a45362e8db61f0c0
Sunbourne <sunsign.av@gmail.com>
2018-01-15 17:55:55 +0200

    Added Script for active page styling

--- scripts/script.js
+++ scripts/script.js
@@ -13,3 +13,38 @@ burger.onclick = function (){
 		burger.className = '';
 	}
 };
+
+function extractPageName(hrefString)
+{
+	var arr = hrefString.split('/');
+	return  (arr.length<2) ? hrefString : arr[arr.length-2].toLowerCase() + arr[arr.length-1].toLowerCase();
+}
+
+function setActiveMenu(arr, crtPage)
+{
+	for (var i=0; i<arr.length; i++)
+	{
+		if(extractPageName(arr[i].href) == crtPage)
+		{
+			if (arr[i].parentNode.tagName != "DIV")
+			{
+				arr[i].className = "current";
+				arr[i].parentNode.className = "current";
+			}
+		}
+	}
+}
+
+function setPage()
+{
+	hrefString = document.location.href ? document.location.href : document.location;
+
+	if (document.getElementById("main-menu")!=null)
+		setActiveMenu(document.getElementById("main-menu").getElementsByTagName("a"), extractPageName(hrefString));
+}
+
+
+window.onload=function()
+{
+	setPage();
+}
